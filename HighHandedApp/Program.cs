53e4c25c81e8d209b501c0e6bcf38a16f7867f27
keith <hybridwebtech@gmail.com>
2018-12-17 17:43:10 -0700

    Initial commit

---
+++ HighHandedApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HighHandedApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
