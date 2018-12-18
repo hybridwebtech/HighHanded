6adaf060b1f7d7da4c30ffc4c13892d389edafc0
keith <hybridwebtech@gmail.com>
2018-12-17 20:27:23 -0700

    Verify valid hands

--- HighHandedApp/PlayHand.cs
+++ HighHandedApp/PlayHand.cs
@@ -1,25 +1,51 @@
 using System;
+using System.Linq;
 
 namespace HighHandedApp
 {
     public class PlayHand
     {
+        public static string INVALID_INPUT_MSG 
+            = "Invalid input: null or empty string";
+
+        public static string INSUFFICIENT_DATA_MSG
+            = "Input data must contain exactly eleven characters";
+        
+        public static string TWO_HANDS_BETTER_THAN_ONE_MSG 
+            = "Two hands must be specified, only one was detected";
+
+        public static string AMBIGUOUS_INPUT_MSG
+            = "Ambiguous input: more than two hands detected";
+
+        public static string MALFORMED_INPUT_MSG
+            = "Malformed input: both hands must contain five cards.";
+
+        public static string ILLEGAL_INPUT_MSG
+            = "Illegal character in hand";
+
+        public static string LegalCharacters = "*23456789TJQKA";
+            
         public string Play(string input)
         {
             if (string.IsNullOrWhiteSpace(input))
             {
-                return "Invalid input: null or empty string";
+                return INVALID_INPUT_MSG;
+            }
+
+            if (input.Length != 11)
+            {
+                return INSUFFICIENT_DATA_MSG;
             }
 
             string[] hands = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
             if (hands.Length < 2)
             {
-                return "Two hands must be specified, only one was detected";
+                return TWO_HANDS_BETTER_THAN_ONE_MSG;
             }
             else if (hands.Length > 2)
             {
-                return "Ambiguous input: more than two hands detected";
+                return AMBIGUOUS_INPUT_MSG;
             }
 
             string handA = hands[0];
@@ -27,10 +53,26 @@ namespace HighHandedApp
 
             if (handA.Length != 5 || handB.Length != 5)
             {
-                return "Malformed input: both hands must contain five cards.";
+                return MALFORMED_INPUT_MSG;
+            }
+
+            if (!IsValidHand(handA) || !IsValidHand(handB))
+            {
+                return ILLEGAL_INPUT_MSG;
             }
             
             throw new NotImplementedException();
+        }
+
+        public static bool IsValidHand(string hand)
+        {
+            foreach (var c in hand)
+            {
+                if (!LegalCharacters.Contains(c))
+                    return false;
+            }
+
+            return true;
         }
     }
 }
--- HighHandedTests/TestPlayHand.cs
+++ HighHandedTests/TestPlayHand.cs
@@ -11,9 +11,106 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void Test_Null_Input()
         {
-            Assert.Pass();
+            var playHand = new PlayHand();
+
+            string result = playHand.Play(null);
+            
+            Assert.AreEqual( PlayHand.INVALID_INPUT_MSG, result);
+        }
+
+        [Test]
+        public void Test_Empty_Input()
+        {
+            var playHand = new PlayHand();
+
+            string result = playHand.Play("");
+            
+            Assert.AreEqual( PlayHand.INVALID_INPUT_MSG, result);            
+        }
+
+        [Test]
+        public void Test_All_Whitespace()
+        {
+            var playHand = new PlayHand();
+
+            string result = playHand.Play("");
+            
+            Assert.AreEqual( PlayHand.INVALID_INPUT_MSG, result);                        
+        }
+
+        [Test]
+        public void Test_Too_Few_Characters()
+        {
+            var playHand = new PlayHand();
+
+            string result = playHand.Play("12345678");
+            
+            Assert.AreEqual( PlayHand.INSUFFICIENT_DATA_MSG, result);                                    
+        }
+        
+        [Test]
+        public void Test_Too_Many_Characters()
+        {
+            var playHand = new PlayHand();
+
+            string result = playHand.Play("123456789ABC");
+            
+            Assert.AreEqual( PlayHand.INSUFFICIENT_DATA_MSG, result);                                    
+        }
+        
+        [Test]
+        public void Test_Cannot_Tokenize_Input()
+        {
+            var playHand = new PlayHand();
+
+            string result = playHand.Play("123456789AB");
+            
+            Assert.AreEqual( PlayHand.TWO_HANDS_BETTER_THAN_ONE_MSG, result);                                    
+        }
+        
+        [Test]
+        public void Test_Too_Many_Tokens()
+        {
+            var playHand = new PlayHand();
+
+            string result = playHand.Play("1234 678 AB");
+            
+            Assert.AreEqual( PlayHand.AMBIGUOUS_INPUT_MSG, result);                                    
+        }
+        
+        [Test]
+        public void Test_Unsymmetric_Tokens()
+        {
+            var playHand = new PlayHand();
+
+            string result = playHand.Play("123456 1234");
+            
+            Assert.AreEqual( PlayHand.MALFORMED_INPUT_MSG, result);                                    
+        }
+
+        [Test]
+        public void Test_IsValid_Hand_With_Illegal_Input()
+        {
+            Assert.IsFalse(PlayHand.IsValidHand("1234%"));
+        }
+        
+        [Test]
+        public void Test_IsValid_Hand_With_Legal_Input()
+        {
+            Assert.IsTrue(PlayHand.IsValidHand("*2345"));
+            Assert.IsTrue(PlayHand.IsValidHand("AAKKK"));
+        }        
+        
+        [Test]
+        public void Test_Illegal_Charaters()
+        {
+            var playHand = new PlayHand();
+
+            string result = playHand.Play("*2345 *234%");
+            
+            Assert.AreEqual( PlayHand.ILLEGAL_INPUT_MSG, result);                                    
         }
     }
 }
